e36408bb92b8d8dadef8fe3472b105f690c49a75
rinaldi <darksideofbotix@gmail.com>
2020-11-06 13:06:30 +0700

    edit create blokir khuss

--- app/BlokirKhusus.php
+++ app/BlokirKhusus.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class BlokirKhusus extends Model
 {
     protected $table = 'blokir_khusus';
+
+    public function setNoIdentitasAttribute($value)
+    {
+        $this->attributes['no_identitas'] = preg_replace('/[^0-9a-zA-Z]/', '', $value);
+    }
 }

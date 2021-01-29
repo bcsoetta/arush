89f749f5361a7dfe1fe414e61cd1da9d8a4d04e9
rinaldi <darksideofbotix@gmail.com>
2021-01-30 04:23:05 +0700

    kurs fiskal salah ejaan kbbi

--- app/Http/Controllers/KursController.php
+++ app/Http/Controllers/KursController.php
@@ -39,7 +39,7 @@ class KursController extends Controller
      */
     public function store(Request $request)
     {
-        $this->validate($request,[
+        $this->validate($request, [
             'code' => 'required',
             'label' => 'required',
             'nilai' => 'required',
@@ -57,7 +57,6 @@ class KursController extends Controller
 
         Alert::success('Berhasil');
         return back();
-
     }
 
     /**
@@ -92,20 +91,20 @@ class KursController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $this->validate($request,[
+        $this->validate($request, [
             'code' => 'required',
             'label' => 'required',
             'nilai' => 'required',
             'berlaku' => 'required',
             'sampai' => 'required',
         ]);
-        
+
         $kurs = Kurs::findOrFail($id);
         $kurs->code = $request->code;
         $kurs->label = $request->label;
         $kurs->nilai = $request->nilai;
-        $kurs->tgl_awal = date_create_from_format('d-m-Y',$request->berlaku);
-        $kurs->tgl_akhir = date_create_from_format('d-m-Y',$request->sampai);
+        $kurs->tgl_awal = date_create_from_format('d-m-Y', $request->berlaku);
+        $kurs->tgl_akhir = date_create_from_format('d-m-Y', $request->sampai);
         $kurs->save();
 
         Alert::success('Berhasil Update');
@@ -134,87 +133,86 @@ class KursController extends Controller
 
     public function updateAllx()
     {
-        $queries=array();
-        $validity=array();
+        $queries = array();
+        $validity = array();
 
-        $months=array(
+        $months = array(
             'January', 'Januari', 'February', 'Pebruari', 'Februari', 'March', 'Maret', 'April', 'May', 'Mei', 'June', 'Juni',
             'July', 'Juli', 'August', 'Agustus', 'September', 'October', 'Oktober', 'November', 'Nopember', 'December', 'Desember'
         );
-        $month_ids=array(
+        $month_ids = array(
             '01', '01', '02', '02', '02', '03', '03', '04', '05', '05', '06', '06', '07', '07', '08', '08', '09', '10', '10',
             '11', '11', '12', '12'
         );
 
         $doc = new \DOMDocument;
         dd(@$doc->loadHTML(file_get_contents('https://www.fiskal.kemenkeu.go.id/dw-kurs-db.asp')));
-        if(@$doc->loadHTML(file_get_contents('https://fiskal.kemenkeu.go.id/dw-kurs-db.asp')))
-            {
-                dd($doc);
-                $tds=$doc->getElementsByTagName('td');
+        if (@$doc->loadHTML(file_get_contents('https://fiskal.kemenkeu.go.id/dw-kurs-db.asp'))) {
+            dd($doc);
+            $tds = $doc->getElementsByTagName('td');
 
-                $valuta;
+            $valuta;
 
-                $ccc= array() ;
+            $ccc = array();
 
-                foreach ($tds as $td) {
-                    $value=$td->nodeValue;
-                    
-                    $matches;
-                    if(preg_match('/\(([^\)]*)\)/', $value, $matches)){
-                        $valuta=$matches[1];
-                        
-                        continue;
-                    }
+            foreach ($tds as $td) {
+                $value = $td->nodeValue;
 
-                    if(isset($valuta)){
-                        $value = (float)$this->grab_number($td->nodeValue)*1.0;
-                        
-                        if(strlen($valuta)===3){
-                            if($valuta == "JPY"){$value*=0.01;};
-                            // $data = Kurs::where('code', $valuta)->first();
-                            // $data->nilai = $value;
-                            // $data->save();
-                            DB::table('kurs')
-                                    ->where('code', $valuta)
-                                    ->update(['nilai' => $value]);
-                            $valuta='';
-                        }
-                    }
+                $matches;
+                if (preg_match('/\(([^\)]*)\)/', $value, $matches)) {
+                    $valuta = $matches[1];
+
+                    continue;
                 }
 
+                if (isset($valuta)) {
+                    $value = (float)$this->grab_number($td->nodeValue) * 1.0;
 
-
-
-                $cnt=0;
-                $started=0;
-                $ps=$doc->getElementsByTagName('em')->item(0);
-
-                $textTanggal = trim($ps->nodeValue);
-
-                $textTanggal = explode(':', $textTanggal);
-
-                $tgl = explode('-',$textTanggal[1]);
-
-                $tgl_awal = trim($tgl[0]);
-                $tgl_akhir = trim($tgl[1]);
-
-                $tgl_awal = str_replace($months, $month_ids, $tgl_awal);
-                $tgl_akhir = str_replace($months, $month_ids, $tgl_akhir);
-
-                $tgl_awal = date_create_from_format("d m Y",$tgl_awal);
-                $tgl_akhir = date_create_from_format("d m Y",$tgl_akhir);
-                DB::table('kurs')->update(['tgl_awal' => $tgl_awal]);
-                DB::table('kurs')->update(['tgl_akhir' => $tgl_akhir]);
-
-            }else{
-                Alert::error('Failed to load url fiskal');
+                    if (strlen($valuta) === 3) {
+                        if ($valuta == "JPY") {
+                            $value *= 0.01;
+                        };
+                        // $data = Kurs::where('code', $valuta)->first();
+                        // $data->nilai = $value;
+                        // $data->save();
+                        DB::table('kurs')
+                            ->where('code', $valuta)
+                            ->update(['nilai' => $value]);
+                        $valuta = '';
+                    }
+                }
             }
 
-            Alert::success('Update kurs');
 
-            return redirect()->back();
 
+
+            $cnt = 0;
+            $started = 0;
+            $ps = $doc->getElementsByTagName('em')->item(0);
+
+            $textTanggal = trim($ps->nodeValue);
+
+            $textTanggal = explode(':', $textTanggal);
+
+            $tgl = explode('-', $textTanggal[1]);
+
+            $tgl_awal = trim($tgl[0]);
+            $tgl_akhir = trim($tgl[1]);
+
+            $tgl_awal = str_replace($months, $month_ids, $tgl_awal);
+            $tgl_akhir = str_replace($months, $month_ids, $tgl_akhir);
+
+            $tgl_awal = date_create_from_format("d m Y", $tgl_awal);
+            $tgl_akhir = date_create_from_format("d m Y", $tgl_akhir);
+            DB::table('kurs')->update(['tgl_awal' => $tgl_awal]);
+            DB::table('kurs')->update(['tgl_akhir' => $tgl_akhir]);
+        } else {
+            Alert::error('Failed to load url fiskal');
+        }
+
+        Alert::success('Update kurs');
+
+        return redirect()->back();
     }
 
     public function grab_number($kmk)
@@ -225,126 +223,129 @@ class KursController extends Controller
         // dd($kmk);
     }
 
-    public function updateAll() {	
-		$monthLookup = array(
-			'Januari'	=> '01',
-			'January'	=> '01',
-			'Februari'	=> '02',
-			'February'	=> '02',
-			'Maret'		=> '03',
-			'March'		=> '03',
-			'April'		=> '04',
-			'Mei'		=> '05',
-			'May'		=> '05',
-			'Juni'		=> '06',
-			'June'		=> '06',
-			'Juli'		=> '07',
-			'July'		=> '07',
-			'Agustus'	=> '08',
-			'August'	=> '08',
-			'September'	=> '09',
-			'Oktober'	=> '10',
-			'October'	=> '10',
-			'Nopember'	=> '11',
-			'November'	=> '11',
-			'Desember'	=> '12',
-			'December'	=> '12'
-			);	
-		// ssl context (BYPASS SSL)
-		$arrContextOptions=array(
-			"ssl"=>array(
-				"cafile" => "/home/services/kurs_kemenkeu.crt",
-				"verify_peer"=>false,
-				"verify_peer_name"=>false,
-			),
-		);  
-		// source data
+    public function updateAll()
+    {
+        $monthLookup = array(
+            'Januari'    => '01',
+            'January'    => '01',
+            'Februari'    => '02',
+            'February'    => '02',
+            'Pebruary'    => '02',
+            'Pebruari'    => '02',
+            'Maret'        => '03',
+            'March'        => '03',
+            'April'        => '04',
+            'Mei'        => '05',
+            'May'        => '05',
+            'Juni'        => '06',
+            'June'        => '06',
+            'Juli'        => '07',
+            'July'        => '07',
+            'Agustus'    => '08',
+            'August'    => '08',
+            'September'    => '09',
+            'Oktober'    => '10',
+            'October'    => '10',
+            'Nopember'    => '11',
+            'November'    => '11',
+            'Desember'    => '12',
+            'December'    => '12'
+        );
+        // ssl context (BYPASS SSL)
+        $arrContextOptions = array(
+            "ssl" => array(
+                "cafile" => "/home/services/kurs_kemenkeu.crt",
+                "verify_peer" => false,
+                "verify_peer_name" => false,
+            ),
+        );
         // source data
-		try {
-			//code...
-			$html = file_get_contents('https://fiskal.kemenkeu.go.id/informasi-publik/kurs-pajak', false, stream_context_create($arrContextOptions) );
-		} catch (\Exception $e) {
-			// return empty data, will be interpreted as service unavailable
-			return $e;
-		}
-        
-        
-        
-		// echo $html;
-        
-		// grab tanggal awal dan akhir
+        // source data
+        try {
+            //code...
+            $html = file_get_contents('https://fiskal.kemenkeu.go.id/informasi-publik/kurs-pajak', false, stream_context_create($arrContextOptions));
+        } catch (\Exception $e) {
+            // return empty data, will be interpreted as service unavailable
+            return $e;
+        }
+
+
+
+        // echo $html;
+
+        // grab tanggal awal dan akhir
         // $patTanggal = '/Tanggal Berlaku\:\s(\d{1,2})\s+(\w+)\s+(\d{4})\s\-\s(\d{1,2})\s+(\w+)\s+(\d{4})/i';
         $patTanggal = '/Tanggal Berlaku\:\s+(\d{1,2})\s+(\w+)\s+(\d{4})\s+\-\s+(\d{1,2})\s+(\w+)\s+(\d{4})/i';
         $result = preg_match($patTanggal, $html, $matches);
-        
-		if (count($matches) >= 6) {
-			// fix tanggal
-			if (strlen($matches[1]) == 1)
-				$matches[1] = '0'.$matches[1];
 
-			if (strlen($matches[4]) == 1)
-				$matches[4] = '0'.$matches[4];	
+        if (count($matches) >= 6) {
+            // fix tanggal
+            if (strlen($matches[1]) == 1)
+                $matches[1] = '0' . $matches[1];
 
-			$retData = array(
-				'dateStart'	=> $matches[3].'-'.$monthLookup[$matches[2]].'-'.$matches[1],
-				'dateEnd'	=> $matches[6].'-'.$monthLookup[$matches[5]].'-'.$matches[4],
-				'data' => array()
-				);
-		} else{
-			Alert::error('no data');
+            if (strlen($matches[4]) == 1)
+                $matches[4] = '0' . $matches[4];
+
+            $retData = array(
+                'dateStart'    => $matches[3] . '-' . $monthLookup[$matches[2]] . '-' . $matches[1],
+                'dateEnd'    => $matches[6] . '-' . $monthLookup[$matches[5]] . '-' . $matches[4],
+                'data' => array()
+            );
+        } else {
+            Alert::error('no data');
             return redirect()->back();
         }
-		// grab data asli (KODE KURS + NILAI TUKARNYA)
+        // grab data asli (KODE KURS + NILAI TUKARNYA)
         // $patKurs = '/\(([A-Z]{3})\).+.+>(.+)\s<img/';
         // $patKurs = '/\((\w{3})\)<\/td>\s+<td.+>\s+<img.+\/>\s+([0-9\,\.]+)<\/td>/';
         $patKurs = '/\((\w{3})\)<\/td>\s.+\s.+\s.+>([0-9]{1,3}\.[0-9]{3}\,[0-9]{2,})<\/div>/';
 
-		$result = preg_match_all($patKurs, $html, $matches);
-		// dd($matches);
+        $result = preg_match_all($patKurs, $html, $matches);
+        // dd($matches);
 
-		if (count($matches) > 2) {
-			// dump it all?
-			for ($i = 0; $i < count($matches[0]); $i++) {
-				$kdValuta = $matches[1][$i];
+        if (count($matches) > 2) {
+            // dump it all?
+            for ($i = 0; $i < count($matches[0]); $i++) {
+                $kdValuta = $matches[1][$i];
 
-				// FIX: AUTO-DETECT NUMBER FORMAT. CHECK RIGHT MOST
-				$comma = substr($matches[2][$i], -3, 1);
+                // FIX: AUTO-DETECT NUMBER FORMAT. CHECK RIGHT MOST
+                $comma = substr($matches[2][$i], -3, 1);
 
-				if ($comma == ',') {
-					// some idiot at BKF decides to use comma as decimal separator					
-					$nilai = str_replace('.', '', $matches[2][$i]);
-					$nilai = str_replace(',', '.', $nilai);
-				} else if ($comma == '.') {
-					// the usual format. just remove all commas
-					$nilai = str_replace(',', '', $matches[2][$i]);
-				} else {
-					// must be error. throw something
-					throw new \Exception("Unknown decimal separator '{$comma}'", 400);
-				}
-				
+                if ($comma == ',') {
+                    // some idiot at BKF decides to use comma as decimal separator					
+                    $nilai = str_replace('.', '', $matches[2][$i]);
+                    $nilai = str_replace(',', '.', $nilai);
+                } else if ($comma == '.') {
+                    // the usual format. just remove all commas
+                    $nilai = str_replace(',', '', $matches[2][$i]);
+                } else {
+                    // must be error. throw something
+                    throw new \Exception("Unknown decimal separator '{$comma}'", 400);
+                }
 
-				$kurs = $nilai * 1;
 
-				// for JPY, divide further by 100
-				if ($kdValuta == 'JPY')
-					$kurs /= 100.0;
+                $kurs = $nilai * 1;
 
-				// echo $kdValuta . ' = ' . sprintf("%.4f", $kurs) . "\n";
+                // for JPY, divide further by 100
+                if ($kdValuta == 'JPY')
+                    $kurs /= 100.0;
 
-				// just plug it in I guess
-				$retData['data'][$kdValuta] = sprintf("%.4f", $kurs);
-			}	
-		} else {
-		    Alert::error('no data');
+                // echo $kdValuta . ' = ' . sprintf("%.4f", $kurs) . "\n";
+
+                // just plug it in I guess
+                $retData['data'][$kdValuta] = sprintf("%.4f", $kurs);
+            }
+        } else {
+            Alert::error('no data');
             return redirect()->back();
         }
 
         // dd($retData);
 
-		if (isset($retData)){
+        if (isset($retData)) {
             foreach ($retData['data'] as $key => $value) {
                 $valuta = DB::table('kurs')->where('code', $key)->get();
-                if(count($valuta) > 0){
+                if (count($valuta) > 0) {
                     //jika sudah ada
                     DB::table('kurs')
                         ->where('code', $key)
@@ -352,18 +353,18 @@ class KursController extends Controller
                 } else {
                     //jika belum ada ada
                     DB::table('kurs')->insert(
-                        ['code'=> $key, 'label'=> $key, 'nilai'=> $value]
+                        ['code' => $key, 'label' => $key, 'nilai' => $value]
                     );
                 }
             }
-                DB::table('kurs')->update(['tgl_awal' => $retData['dateStart']]);
-                DB::table('kurs')->update(['tgl_akhir' => $retData['dateEnd']]);
-                
-                Alert::success('Update kurs');
-                return redirect()->back();
-        }else {
-		    Alert::error('no data');
+            DB::table('kurs')->update(['tgl_awal' => $retData['dateStart']]);
+            DB::table('kurs')->update(['tgl_akhir' => $retData['dateEnd']]);
+
+            Alert::success('Update kurs');
+            return redirect()->back();
+        } else {
+            Alert::error('no data');
             return redirect()->back();
         }
-	}
+    }
 }
--- resources/views/dokumen/create.blade.php
+++ resources/views/dokumen/create.blade.php
@@ -19,46 +19,47 @@ Rekam Dokumen
         <h3 class="panel-title">Rekam Dokumen</h3>
     </div>
     <div class="panel-body">
+        <a href="{{ url()->previous() }}"><button class="btn btn-primary" style="margin: 15px; margin-left: 0px;">Kembali</button></a>
         <form method="POST" action="{{ route('dokumen.store') }}">
-        {{ csrf_field() }}
-        {{-- parsial form --}}
-        @include('partial._form_dokumen')
+            {{ csrf_field() }}
+            {{-- parsial form --}}
+            @include('partial._form_dokumen')
         </form>
     </div>
 </div> {{-- end-panel --}}
 <!-- Modal -->
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog modal-lg" role="document" style="width: 85%;">
-    <div class="modal-content">
-      <div class="modal-header">
-        <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button> -->
-        <h4 class="modal-title" id="myModalLabel">Dokumen Yang belum diserahkan</h4>
-      </div>
-      <div class="modal-body">
-        <table class="table table-bordered" id="mytable1">
-            <thead>
-                <tr>
-                    <th>No Daftar</th>
-                    <th>Tgl</th>
-                    <th>Importir</th>
-                    <th>NPWP</th>
-                    <th>HAWB</th>
-                    <th>Tgl</th>
-                    <th>SPPB</th>
-                    <th>Tgl</th>
-                    <th>Lewat hari</th>
-                </tr>
-            </thead>
-            <tbody>
-                
-            </tbody>
-        </table>
-      </div>
-      <div class="modal-footer">
-        <!-- <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> -->
-      </div>
+    <div class="modal-dialog modal-lg" role="document" style="width: 85%;">
+        <div class="modal-content">
+            <div class="modal-header">
+                <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button> -->
+                <h4 class="modal-title" id="myModalLabel">Dokumen Yang belum diserahkan</h4>
+            </div>
+            <div class="modal-body">
+                <table class="table table-bordered" id="mytable1">
+                    <thead>
+                        <tr>
+                            <th>No Daftar</th>
+                            <th>Tgl</th>
+                            <th>Importir</th>
+                            <th>NPWP</th>
+                            <th>HAWB</th>
+                            <th>Tgl</th>
+                            <th>SPPB</th>
+                            <th>Tgl</th>
+                            <th>Lewat hari</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+            <div class="modal-footer">
+                <!-- <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> -->
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 @endsection
 
@@ -103,10 +104,10 @@ Rekam Dokumen
 
     $(function() {
         $("#importir_nm").autocomplete({
-            source:  "{{route('auto.importir')}}",
+            source: "{{route('auto.importir')}}",
             minLength: 1,
-            autoFocus:true,
-            select: function( event, ui ) {
+            autoFocus: true,
+            select: function(event, ui) {
                 $('#importir_nm').val(ui.item.nama);
                 $('#importir_npwp').val(ui.item.npwp);
                 $('#importir_alamat').val(ui.item.alamat);
@@ -116,10 +117,10 @@ Rekam Dokumen
 
     $(function() {
         $("#ppjk_nm").autocomplete({
-            source:  "{{route('auto.ppjk')}}",
+            source: "{{route('auto.ppjk')}}",
             minLength: 1,
-            autoFocus:true,
-            select: function( event, ui ) {
+            autoFocus: true,
+            select: function(event, ui) {
                 $('#ppjk_nm').val(ui.item.nama);
                 $('#ppjk_npwp').val(ui.item.npwp);
                 $('#ppjk_alamat').val(ui.item.alamat);
@@ -127,36 +128,36 @@ Rekam Dokumen
         });
     });
 
-    $(document).on('change', function(){
+    $(document).on('change', function() {
         var npwp = $('#importir_npwp').val();
 
-        if(npwp !== null && npwp !== ''  && npwp!==undefined){
+        if (npwp !== null && npwp !== '' && npwp !== undefined) {
             $.ajax({
-                url: "{{url('cek-dokumen-npwp')}}"+ '/' + npwp,
+                url: "{{url('cek-dokumen-npwp')}}" + '/' + npwp,
                 type: "GET",
                 dataType: 'json',
-                success: function (data) {
+                success: function(data) {
                     // $('#myModal').modal('show');
                     $('#myModal').modal({
                         backdrop: 'static'
                     });
-                    $.each(data,function(k, v){
-                        var classD = v.selisih_hari > 3 ? 'danger': '';
+                    $.each(data, function(k, v) {
+                        var classD = v.selisih_hari > 3 ? 'danger' : '';
 
                         $("#mytable1 > tbody").append(
-                            "<tr class='"+ classD +"'>"+
-                            "<td>" + v.daftar_no +"</td>"+
-                            "<td>" + v.daftar_tgl +"</td>"+
-                            "<td>" + v.importir_nm +"</td>"+
-                            "<td>" + v.importir_npwp +"</td>"+
-                            "<td>" + v.hawb_no +"</td>"+
-                            "<td>" + v.hawb_tgl +"</td>"+
-                            "<td>" + v.sppb.no_sppb +"</td>"+
-                            "<td>" + v.sppb.created_at +"</td>"+
-                            "<td>" + v.selisih_hari +"</td>"+
-                        "</tr>");
+                            "<tr class='" + classD + "'>" +
+                            "<td>" + v.daftar_no + "</td>" +
+                            "<td>" + v.daftar_tgl + "</td>" +
+                            "<td>" + v.importir_nm + "</td>" +
+                            "<td>" + v.importir_npwp + "</td>" +
+                            "<td>" + v.hawb_no + "</td>" +
+                            "<td>" + v.hawb_tgl + "</td>" +
+                            "<td>" + v.sppb.no_sppb + "</td>" +
+                            "<td>" + v.sppb.created_at + "</td>" +
+                            "<td>" + v.selisih_hari + "</td>" +
+                            "</tr>");
                     })
-                    
+
                 }
             });
         } else {
@@ -164,7 +165,5 @@ Rekam Dokumen
         }
 
     });
-
-
 </script>
 @endsection

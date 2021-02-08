9a3ff6594c481689cb62da9cbd3e04d7c52b6137
rinaldi <darksideofbotix@gmail.com>
2021-02-08 09:18:28 +0700

    ganti tingkat pemeriksaaan 10%

--- resources/views/instruksipemeriksaan/create.blade.php
+++ resources/views/instruksipemeriksaan/create.blade.php
@@ -48,7 +48,7 @@ Rekam IP
                         <label for="tingkat_periksa" class="col-md-4 control-label">Tingkat Pemeriksa</label>
 
                         <div class="col-md-6">
-                            <input id="tingkat_periksa" type="text" class="form-control" name="tingkat_periksa" value="{{ old('tingkat_periksa') ? old('tingkat_periksa') : '30%'  }}">
+                            <input id="tingkat_periksa" type="text" class="form-control" name="tingkat_periksa" value="{{ old('tingkat_periksa') ? old('tingkat_periksa') : '10%'  }}">
 
                             @if ($errors->has('tingkat_periksa'))
                             <span class="help-block">
@@ -59,96 +59,96 @@ Rekam IP
                     </div>
 
                     {{-- <div class="form-group{{ $errors->has('jumlah_kemasan_diperiksa') ? ' has-error' : '' }}">
-                                <label for="jumlah_kemasan_diperiksa" class="col-md-4 control-label">Jumlah kemasan yang harus diperiksa</label>
+                    <label for="jumlah_kemasan_diperiksa" class="col-md-4 control-label">Jumlah kemasan yang harus diperiksa</label>
 
-                                <div class="col-md-6">
-                                    <input id="user_name" type="text" class="form-control" name="jumlah_kemasan_diperiksa" value="{{ old('jumlah_kemasan_diperiksa') }}">
+                    <div class="col-md-6">
+                        <input id="user_name" type="text" class="form-control" name="jumlah_kemasan_diperiksa" value="{{ old('jumlah_kemasan_diperiksa') }}">
 
-                                    @if ($errors->has('jumlah_kemasan_diperiksa'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('jumlah_kemasan_diperiksa') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group{{ $errors->has('nomor_kontainer_diperiksa') ? ' has-error' : '' }}">
-                                <label for="nomor_kontainer_diperiksa" class="col-md-4 control-label">Nomor kontainer yang diperiksa</label>
-
-                                <div class="col-md-6">
-                                    <input id="nomor_kontainer_diperiksa" type="text" class="form-control" name="nomor_kontainer_diperiksa" value="{{ old('nomor_kontainer_diperiksa') }}">
-
-                                    @if ($errors->has('nomor_kontainer_diperiksa'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('nomor_kontainer_diperiksa') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group{{ $errors->has('nomor_kemasan_diperiksa') ? ' has-error' : '' }}">
-                                <label for="nomor_kemasan_diperiksa" class="col-md-4 control-label">Nomor kemasan yang diperiksa</label>
-
-                                <div class="col-md-6">
-                                    <input id="nomor_kemasan_diperiksa" type="text" class="form-control" name="nomor_kemasan_diperiksa" value="{{ old('nomor_kemasan_diperiksa') }}">
-
-                                    @if ($errors->has('nomor_kemasan_diperiksa'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('nomor_kemasan_diperiksa') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div> --}}
-
-                    <div class="form-group{{ $errors->has('aju_contoh') ? ' has-error' : '' }}">
-                        <label for="aju_foto" class="col-md-4 control-label">Ajukan contoh(ya/tidak)</label>
-
-                        <div class="col-md-6">
-                            <select class="form-control" name="aju_contoh" id="aju_contoh">
-                                <option value=""></option>
-                                <option value="YA"> YA</option>
-                                <option value="TIDAK"> TIDAK</option>
-                            </select>
-
-                            @if ($errors->has('aju_contoh'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('aju_contoh') }}</strong>
-                            </span>
-                            @endif
-                        </div>
+                        @if ($errors->has('jumlah_kemasan_diperiksa'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('jumlah_kemasan_diperiksa') }}</strong>
+                        </span>
+                        @endif
                     </div>
-
-                    <div class="form-group{{ $errors->has('aju_foto') ? ' has-error' : '' }}">
-                        <label for="aju_foto" class="col-md-4 control-label">Ajukan foto(ya/tidak)</label>
-
-                        <div class="col-md-6">
-                            <select class="form-control" name="aju_foto" id="aju_foto">
-                                <option value=""></option>
-                                <option value="YA"> YA</option>
-                                <option value="TIDAK"> TIDAK</option>
-                            </select>
-
-                            @if ($errors->has('aju_foto'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('aju_foto') }}</strong>
-                            </span>
-                            @endif
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <div class="col-md-4 col-md-offset-3">
-                            <button type="submit" class="btn btn-primary" onclick="konfirm()">
-                                Simpan (Pemeriksa akan dipilih secara random)
-                            </button>
-
-                        </div>
-                    </div>
-                    </form>
                 </div>
+
+                <div class="form-group{{ $errors->has('nomor_kontainer_diperiksa') ? ' has-error' : '' }}">
+                    <label for="nomor_kontainer_diperiksa" class="col-md-4 control-label">Nomor kontainer yang diperiksa</label>
+
+                    <div class="col-md-6">
+                        <input id="nomor_kontainer_diperiksa" type="text" class="form-control" name="nomor_kontainer_diperiksa" value="{{ old('nomor_kontainer_diperiksa') }}">
+
+                        @if ($errors->has('nomor_kontainer_diperiksa'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('nomor_kontainer_diperiksa') }}</strong>
+                        </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group{{ $errors->has('nomor_kemasan_diperiksa') ? ' has-error' : '' }}">
+                    <label for="nomor_kemasan_diperiksa" class="col-md-4 control-label">Nomor kemasan yang diperiksa</label>
+
+                    <div class="col-md-6">
+                        <input id="nomor_kemasan_diperiksa" type="text" class="form-control" name="nomor_kemasan_diperiksa" value="{{ old('nomor_kemasan_diperiksa') }}">
+
+                        @if ($errors->has('nomor_kemasan_diperiksa'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('nomor_kemasan_diperiksa') }}</strong>
+                        </span>
+                        @endif
+                    </div>
+                </div> --}}
+
+                <div class="form-group{{ $errors->has('aju_contoh') ? ' has-error' : '' }}">
+                    <label for="aju_foto" class="col-md-4 control-label">Ajukan contoh(ya/tidak)</label>
+
+                    <div class="col-md-6">
+                        <select class="form-control" name="aju_contoh" id="aju_contoh">
+                            <option value=""></option>
+                            <option value="YA"> YA</option>
+                            <option value="TIDAK"> TIDAK</option>
+                        </select>
+
+                        @if ($errors->has('aju_contoh'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('aju_contoh') }}</strong>
+                        </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group{{ $errors->has('aju_foto') ? ' has-error' : '' }}">
+                    <label for="aju_foto" class="col-md-4 control-label">Ajukan foto(ya/tidak)</label>
+
+                    <div class="col-md-6">
+                        <select class="form-control" name="aju_foto" id="aju_foto">
+                            <option value=""></option>
+                            <option value="YA"> YA</option>
+                            <option value="TIDAK"> TIDAK</option>
+                        </select>
+
+                        @if ($errors->has('aju_foto'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('aju_foto') }}</strong>
+                        </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-md-4 col-md-offset-3">
+                        <button type="submit" class="btn btn-primary" onclick="konfirm()">
+                            Simpan (Pemeriksa akan dipilih secara random)
+                        </button>
+
+                    </div>
+                </div>
+                </form>
             </div>
         </div>
     </div>
+</div>
 </div>
 </div> {{-- end-panel --}}
 
